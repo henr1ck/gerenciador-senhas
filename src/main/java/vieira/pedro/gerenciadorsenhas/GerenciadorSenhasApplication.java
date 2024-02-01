746b4c28efad952a703c753c3cdebefe97a9d23e
Pedro Henrique <pedrohenrick.dev@gmail.com>
2024-02-01 15:12:46 -0300

    First Commit

---
+++ src/main/java/vieira/pedro/gerenciadorsenhas/GerenciadorSenhasApplication.java
@@ -0,0 +1,13 @@
+package vieira.pedro.gerenciadorsenhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorSenhasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorSenhasApplication.class, args);
+	}
+
+}
